4012aa40104d8067c9e6d7d9100909123bd0d908
shenghua liang <381400744@qq.com>
2022-02-24 11:17:10 +0800

    视图引擎压测Demo

---
+++ ViewEngineStressTest/Program.cs
@@ -0,0 +1,6 @@
+﻿using Furion.ViewEngine;
+ViewEngine _viewEngine = new ViewEngine();
+for (int i = 0; i < 100000; i++)
+{
+    _viewEngine.RunCompile("Hello @Model.Name", new { Name = "Furion" });
+}
